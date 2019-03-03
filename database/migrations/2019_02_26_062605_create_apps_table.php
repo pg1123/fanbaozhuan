ca98fc972ae7e7163d6624be6e6e4466daa2b557
Yang Liu <yang.liu@mdpi.com>
2019-03-03 13:12:26 +0800

    copy admin views

--- database/migrations/2019_02_26_062605_create_apps_table.php
+++ database/migrations/2019_02_26_062605_create_apps_table.php
@@ -16,6 +16,7 @@ class CreateAppsTable extends Migration
         DB::unprepared('
             CREATE TABLE `apps` (
               `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+              `position` int(10) NOT NULL,
               `cat_id` int(10) NOT NULL,
               `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
               `logo` text COLLATE utf8_unicode_ci,
