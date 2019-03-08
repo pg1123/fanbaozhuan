2e17b9360227d50167800e4d383486000f13cb2c
Yang Liu <yang.liu@mdpi.com>
2019-03-08 16:09:01 +0800

    aq

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Models\App;
+use Illuminate\Support\Facades\Schema; //NEW: Import Schema
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191); //NEW: Increase StringLength
+
         App::deleting(function ($model) {
             $model->next()->decrement('position');
         });
