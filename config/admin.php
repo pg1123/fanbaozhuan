3025303e8828509c147f30988bc66e8f689d0c50
Yang Liu <yang.liu@mdpi.com>
2019-04-14 18:23:59 +0800

    add editor

--- config/admin.php
+++ config/admin.php
@@ -312,6 +312,15 @@ return [
     |
     */
     'extensions' => [
-
+        'wang-editor' => [
+        
+            // 如果要关掉这个扩展，设置为false
+            'enable' => true,
+            
+            // 编辑器的配置
+            'config' => [
+                'uploadImgServer' => '/upload'
+            ]
+        ]
     ],
 ];
